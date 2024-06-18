e886fdf8001c275b2bbc5e45a293a1b4cf351792
Franco Pertusati <pertusatifranco836@gmail.com>
2024-06-18 11:43:29 -0300

    completado el funcionamiento del login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { GameComponent } from './pages/game/game.component';
 import { LoginComponent } from './pages/login/login.component';
-import { SandboxComponent } from './pages/sandbox/sandbox.component';
+// import { SandboxComponent } from './pages/sandbox/sandbox.component';
 
 export const routes: Routes = [
   {
@@ -12,8 +12,8 @@ export const routes: Routes = [
     path: 'game',
     component: GameComponent,
   },
-  {
-    path: 'sandbox',
-    component: SandboxComponent,
-  }
+  // {
+  //   path: 'sandbox',
+  //   component: SandboxComponent,
+  // }
 ];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,17 +1,43 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { WindowsComponent } from '../../shared/components/windows/windows.component';
-import { SimplebtnComponent } from '../../shared/components/simplebtn/simplebtn.component'
+import { SimplebtnComponent } from '../../shared/components/simplebtn/simplebtn.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [WindowsComponent, SimplebtnComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  onSelectUsser = false;
+  onSelectUser = true;
   isHacker = false;
+  winTitle = 'Login to work';
 
-  
+  users = [
+    {
+      image: '',
+      name: 'Juan',
+    },
+    {
+      image: '',
+      name: 'Player',
+    },
+    {
+      image: '',
+      name: 'Boss',
+    },
+  ];
+
+  login(user: string) {
+    this.onSelectUser = false;
+    this.winTitle = 'Login as ' + user;
+  }
+
+  goBack() {
+    this.onSelectUser = true;
+    this.winTitle = 'Login to work'
+  }
+
+
 }
