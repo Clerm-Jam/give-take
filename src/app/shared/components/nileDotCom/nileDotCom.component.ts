0a82fb63c629ee59fbe2f16c8f6c0184786ca397
Jose Arcidiacono <j2arcidiacono@outlook.com>
2024-06-20 17:47:33 -0300

    niledotcom

--- src/app/shared/components/nileDotCom/nileDotCom.component.ts
+++ src/app/shared/components/nileDotCom/nileDotCom.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { WindowsComponent } from '../windows/windows.component';
+import { SimplebtnComponent } from '../simplebtn/simplebtn.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nile-dot-com',
   standalone: true,
-  imports: [],
+  imports: [WindowsComponent, SimplebtnComponent, CommonModule],
   templateUrl: './nileDotCom.component.html',
 })
 export class NileComComponent {
+  byeText = false;
+  value = '';
+  error = false;
 
+  numbers(event: Event) {
+    const input = event.target as HTMLInputElement;
+    input.value = "*".repeat(input.value.length);
+  }
+
+  cardHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.value = input.value;
+    input.value = this.value.slice(0, input.value.length - 1);
+  }
+
+  buyHandler = () => {
+    this.byeText = true;
+  }
+
+  getError() {
+    return this.error
+  }
 }
