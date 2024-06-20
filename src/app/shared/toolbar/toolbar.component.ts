76b6af90bdfddf664b9e981da119724d9a3d3ddb
Franco Pertusati <pertusatifranco836@gmail.com>
2024-06-20 00:12:51 -0300

    agregadas un par de cosas en el dayOne

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { SimplebtnComponent } from '../components/simplebtn/simplebtn.component';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [],
+  imports: [SimplebtnComponent, ],
   templateUrl: './toolbar.component.html',
 })
 export class ToolbarComponent {
-
+  
 }
